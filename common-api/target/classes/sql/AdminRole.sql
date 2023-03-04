7c55042bae5a653d623e9f3d8a0b82302e988f1a
Chenhui Gu <c46gu@uwaterloo.ca>
2023-03-03 20:11:54 -0500

    removed unnecessary files

--- common-api/target/classes/sql/AdminRole.sql
+++
@@ -1,8 +0,0 @@
-insert into admin_role (admin_id, role_id)
-values (1, 1);
-insert into admin_role (admin_id, role_id)
-values (2, 2);
-insert into admin_role (admin_id, role_id)
-values (3, 3);
-insert into admin_role (admin_id, role_id)
-values (4, 4);
--- common-api/target/classes/sql/Exercise.sql
+++
@@ -1,4 +0,0 @@
-insert into exercise (exercise_name) values ('push up');
-insert into exercise (exercise_name) values ('push down');
-insert into exercise (exercise_name) values ('push left');
-insert into exercise (exercise_name) values ('push right');
